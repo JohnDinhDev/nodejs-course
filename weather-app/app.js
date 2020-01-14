37ec317f0ae39a6c71a0ad391fda0620b2f8b88b
John Dinh <37444633+JohnDinhCodes@users.noreply.github.com>
2020-01-14 16:41:45 -0500

    add geocoding api to weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const request = require("request");
 
-const url = `https://api.darksky.net/forecast/e9c47b4c1eca9b1912ab977ef2e0b98e/37.8267,-122.42330
-`;
-
-request({ url, json: true }, (err, response, body) => {
+const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/37.8267,-122.42330`;
+const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`;
+request({ url: darkSkyUrl, json: true }, (err, response, body) => {
 	const currentForecast = response.body.currently;
 	console.log(
 		`${response.body.daily.data[0].summary} It is currently ${currentForecast.temperature} degrees out with ${currentForecast.precipProbability}% of rain.`
 	);
+});
+
+request({ url: mapBoxUrl, json: true }, (err, response) => {
+	const latitude = response.body.features[0].center[1];
+	const longitude = response.body.features[0].center[0];
+	console.log(latitude, longitude);
 });
